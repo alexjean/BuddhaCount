ee3e54946b26d2594608a58abe10a524081acfa5
AlexJean <alexjean1031@gmail.com>
2023-02-24 22:45:29 +0800

    move to kotlin: ItemFragment.kt

---
+++ app/src/main/java/com/tomorrow_eyes/buddhacount/ItemFragment.kt
@@ -0,0 +1,33 @@
+package com.tomorrow_eyes.buddhacount
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class ItemFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.fragment_item_list, container, false)
+        if (view is RecyclerView) {
+            val context = view.getContext()
+            view.layoutManager = LinearLayoutManager(context)
+            val adapter = MyItemRecyclerViewAdapter(ItemContent.ITEMS)
+            view.adapter = adapter
+        }
+        return view
+    }
+
+    companion object {
+        fun newInstance(): ItemFragment {
+            val fragment = ItemFragment()
+            val args = Bundle()
+            fragment.arguments = args
+            return fragment
+        }
+    }
+}
